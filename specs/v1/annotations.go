c974004b2a02dcf21eca9a3b9b2c5e77645aa433
sai.pubg.blog <sai.pubg.blog@gmail.com>
2025-11-20 17:16:20 +0000

    add patch annotations
    Signed-off-by: sai.pubg.blog <sai.pubg.blog@gmail.com>

---
+++ specs/v1/annotations.go
@@ -0,0 +1,9 @@
+package v1
+
+const (
+	// AnnotationPatchImageDigest is the annotation key for the digest of the image's patch image.
+	AnnotationPatchImageDigest = "org.opencontainers.image.patch.digest"
+
+	// AnnotationPatchImageName is the annotation key for the image reference of the image's patch image.
+	AnnotationPatchImageName = "org.opencontainers.image.patch.name"
+)
